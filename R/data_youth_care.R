d68b31144be717ec45fd90aadbfb6ef5395b8365
kate hayes <khayes2@sheffield.ac.uk>
2022-11-10 12:09:18 +0000

    more changes

--- R/data_youth_care.R
+++ R/data_youth_care.R
@@ -27,7 +27,7 @@ care_data %>%
                             "10 to 15 years", "16 years and over")) %>%
   ggplot() +
   geom_area(aes(x = time_period, y = number,
-                group = characteristic, fill = characteristic)) +
+                group = fct_rev(characteristic), fill = characteristic)) +
   scale_x_continuous(name = "") +
   scale_y_continuous(name = "") +
   theme_classic() +
@@ -93,32 +93,36 @@ care_data %>%
 
 care_data %>%
   filter(la_name == "Birmingham", cla_group == "Placement", characteristic != "Total") %>% #
+  filter(characteristic != "Other placements in the community", characteristic != "Other residential settings", characteristic != "Other placements") %>% #
   mutate(number = as.numeric(number)) %>%
   ggplot() +
-  geom_line(aes(x = time_period, y = number,
-                group = characteristic, colour = characteristic)) +
+  geom_area(aes(x = time_period, y = number,
+                group = fct_reorder(characteristic, number, max), fill = fct_reorder(characteristic, number, max)),
+                colour = "black", size = .2) +
   scale_x_continuous(name = "") +
   scale_y_continuous(name = "") + # , expand = c(0, 0), limits = c(0, NA)
   theme_classic() +
-  theme(strip.background = element_blank())
+  theme(strip.background = element_blank()) +
+  scale_fill_brewer(palette = "Blues")
+ggsave(filename = "Output/Graphs/birm_care_byplacetype.png")
 
-care_data %>%
-  filter(la_name == "Birmingham", cla_group == "Placement", characteristic != "Total") %>% #
-  mutate(number = as.numeric(number)) %>%
-  filter(!is.na(number)) %>%
-  mutate(residential = if_else(characteristic == "Residential schools" |
-                                   characteristic == "Other residential settings" |
-                                   characteristic == "Secure units, children's homes and semi-independent living accommodation",
-                                  "residential", "non-residential")) %>%
-  group_by(residential, time_period) %>%
-  mutate(res_number = sum(number)) %>%
-  ggplot() +
-  geom_line(aes(x = time_period, y = res_number,
-                group = residential, colour = residential)) +
-  scale_x_continuous(name = "") +
-  scale_y_continuous(name = "") + # , expand = c(0, 0), limits = c(0, NA)
-  theme_classic() +
-  theme(strip.background = element_blank())
+# care_data %>% i broke this when i tried to change it to area
+#   filter(la_name == "Birmingham", cla_group == "Placement", characteristic != "Total") %>% #
+#   mutate(number = as.numeric(number)) %>%
+#   filter(!is.na(number)) %>%
+#   mutate(residential = if_else(characteristic == "Residential schools" |
+#                                    characteristic == "Other residential settings" |
+#                                    characteristic == "Secure units, children's homes and semi-independent living accommodation",
+#                                   "residential", "non-residential")) %>%
+#   group_by(residential, time_period) %>%
+#   mutate(res_number = sum(number)) %>%
+#   ggplot() +
+#   geom_area(aes(x = time_period, y = number,
+#                 group = residential, colour = residential)) +
+#   scale_x_continuous(name = "") +
+#   scale_y_continuous(name = "") + # , expand = c(0, 0), limits = c(0, NA)
+#   theme_classic() +
+#   theme(strip.background = element_blank())
 
 #########CATEGORY OF NEED######################################################################################################
 
@@ -184,6 +188,26 @@ av_care_convict_data <- care_outcomes_data %>%
   summarise(av_number = mean(number)) %>%
   kable("latex", booktabs = TRUE)
 
+# note - this is JUST ten to 17 year olds
+care_outcomes_data %>%
+  filter(la_name == "Birmingham", cla_group == "Convictions: Children looked after Ages 10 to 17 years") %>%
+  mutate(number = ifelse(number == "c", 0, number)) %>%
+  mutate(number = as.numeric(number)) %>%
+  pivot_wider(id_cols = time_period, names_from = outcome, values_from = number) %>%
+  mutate(`Not convicted, cautioned` = `Total ages 10 to 17 years` - `Convicted or subject to youth cautions, or youth conditional cautions during the year`) %>%
+  select(-`Total ages 10 to 17 years`) %>%
+  pivot_longer(!time_period, names_to = "outcome", values_to = "number")  %>%
+  ggplot() +
+  geom_area(aes(x = time_period, y = number,
+                group = outcome, fill = outcome),
+            colour = "black", size = .2) +
+  scale_x_continuous(name = "") +
+  scale_y_continuous(name = "") + # , expand = c(0, 0), limits = c(0, NA)
+  theme_classic() +
+  theme(strip.background = element_blank()) +
+  scale_fill_brewer(palette = "Blues")
+ggsave(filename = "Output/Graphs/birm_care_conviction.png")
+
 
 # second dataset - missing
 readin_data <- read.csv("/Users/katehayes/temp_data/children-looked-after-in-england-including-adoptions_2021/data/la_children_who_were_missing_or_away_from_placement_during_the_year.csv")
@@ -196,6 +220,28 @@ av_care_missing_data <- care_outcomes_data %>%
   group_by(cla_group) %>%
   summarise(av_number = mean(number)) %>%
   kable("latex", booktabs = TRUE)
+
+
+care_outcomes_data %>%
+  filter(la_name == "Birmingham") %>%
+  mutate(number = ifelse(number == "c" | number == "z", 0, number)) %>%
+  mutate(number = as.numeric(number)) %>%
+  pivot_wider(id_cols = time_period, names_from = cla_group, values_from = number) %>%
+  mutate(`Did not go missing` = `Children looked after during the year` - `Children who had a missing incident during the year`,
+         `Went missing once` = `Children who had a missing incident during the year` - `Children who went missing more than once during the year`) %>%
+  select(time_period, `Did not go missing`, `Went missing once`, `Children who went missing more than once during the year`) %>%
+  pivot_longer(!time_period, names_to = "missing", values_to = "number")  %>%
+  mutate(missing = fct_relevel(missing, "Children who went missing more than once during the year", "Went missing once", "Did not go missing")) %>%
+  ggplot() +
+  geom_area(aes(x = time_period, y = number,
+                group = missing, fill = fct_rev(missing)),
+            colour = "black", size = .2) +
+  scale_x_continuous(name = "") +
+  scale_y_continuous(name = "") + # , expand = c(0, 0), limits = c(0, NA)
+  theme_classic() +
+  theme(strip.background = element_blank()) +
+  scale_fill_brewer(palette = "PuRd")
+ggsave(filename = "Output/Graphs/birm_care_missing.png")
 
 #####################################################################################################
 #########NEW DATA - time of inflow/outflow, duration######################################################################################################
